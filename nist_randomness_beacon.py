ec1c840d983e9bdcbabaada53b97caa676d8a905
Peter Urda <peter.urda@gmail.com>
2015-11-18 10:49:14 -0800

    Tab in

--- nist_randomness_beacon.py
+++ nist_randomness_beacon.py
@@ -17,41 +17,41 @@ class NistBeaconValue(object):
     ):
         """
         :param version:
-        Reported NIST randomness beacon version
+            Reported NIST randomness beacon version
 
         :param frequency:
-        The time interval, in seconds, between expected records
+            The time interval, in seconds, between expected records
 
         :param timestamp:
-        The time the seed value was generated as the number of
-        seconds since January 1, 1970
+            The time the seed value was generated as the number of
+            seconds since January 1, 1970
 
         :param seed_value:
-        A seed value represented as a 64 byte (512-bit) hex string
-        value
+            A seed value represented as a 64 byte (512-bit) hex string
+            value
 
         :param previous_output_value:
-        The SHA-512 hash value for the previous record - 64 byte hex
-        string
+            The SHA-512 hash value for the previous record - 64 byte hex
+            string
 
         :param signature_value:
-        A digital signature (RSA) computed over (in order): version,
-        frequency, timeStamp, seedValue, previousHashValue, errorCode
+            A digital signature (RSA) computed over (in order): version,
+            frequency, timeStamp, seedValue, previousHashValue, errorCode
 
-        Note: Except for version, the hash is on the byte
-        representations and not the string representations of the data
-        values
+            Note: Except for version, the hash is on the byte
+            representations and not the string representations of the data
+            values
 
         :param output_value:
-        The SHA-512 hash of the signatureValue as a 64 byte hex string
+            The SHA-512 hash of the signatureValue as a 64 byte hex string
 
         :param status_code:
-        The status code value:
-            0 - Chain intact, values all good
-            1 - Start of a new chain of values, previous hash value
-                will be all zeroes
-            2 - Time between values is greater than the frequency, but
-                the chain is still intact
+            The status code value:
+                0 - Chain intact, values all good
+                1 - Start of a new chain of values, previous hash value
+                    will be all zeroes
+                2 - Time between values is greater than the frequency, but
+                    the chain is still intact
         """
 
         self.version = version
