11e63fd82b6c01e0335a081bc67ef21196867ebf
Peter Urda <peter.urda@gmail.com>
2015-11-24 01:16:47 -0500

    Init setup.py, sync version number

--- py_nist_beacon/__init__.py
+++ py_nist_beacon/__init__.py
@@ -1,3 +1,3 @@
 from py_nist_beacon.nist_randomness_beacon import NistRandomnessBeacon
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name='py_nist_beacon',
+    packages=['py_nist_beacon'],
+    version='0.1.6',
+
+    description='Python 3 Library to access the NIST Randomness Beacon',
+    long_description=open('README.rst').read(),
+    license='Apache License, Version 2.0',
+
+    author='Peter Urda',
+    author_email='noreply@urda.cc',
+    url='https://github.com/urda/py_nist_beacon',
+
+    install_requires=[
+        # 'requests>=2.8.1'
+        'requests>=2.5.4.1',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+)
