dcd06f7256b585f9aa60986a17ae831736b23462
Peter Urda <peter.urda@gmail.com>
2015-11-23 21:48:50 -0500

    Expose the other API calls, lots of repeated code here

--- py_nist_beacon/nist_randomness_beacon.py
+++ py_nist_beacon/nist_randomness_beacon.py
@@ -20,3 +20,54 @@ class NistRandomnessBeacon(object):
                 return None
         except RequestException:
             return None
+
+    @classmethod
+    def get_next(cls, timestamp: int):
+        try:
+            r = requests.get(
+                "{0}/next/{1}".format(
+                    cls.NIST_BASE_URL,
+                    timestamp,
+                )
+            )
+
+            if r.status_code is requests.codes.OK:
+                return NistRandomnessBeaconValue.from_xml(r.text)
+            else:
+                return None
+        except RequestException:
+            return None
+
+    @classmethod
+    def get_previous(cls, timestamp: int):
+        try:
+            r = requests.get(
+                "{0}/previous/{1}".format(
+                    cls.NIST_BASE_URL,
+                    timestamp,
+                )
+            )
+
+            if r.status_code is requests.codes.OK:
+                return NistRandomnessBeaconValue.from_xml(r.text)
+            else:
+                return None
+        except RequestException:
+            return None
+
+    @classmethod
+    def get_record(cls, timestamp: int):
+        try:
+            r = requests.get(
+                "{0}/{1}".format(
+                    cls.NIST_BASE_URL,
+                    timestamp,
+                )
+            )
+
+            if r.status_code is requests.codes.OK:
+                return NistRandomnessBeaconValue.from_xml(r.text)
+            else:
+                return None
+        except RequestException:
+            return None
