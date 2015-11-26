8dd8d5bd829ae06192f8d0fc5f7c6d6ec318d911
Peter Urda <peter.urda@gmail.com>
2015-11-25 23:19:33 -0500

    Stubs, going to rename file

---
+++ scripts/version_update.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+def update_package(new_version: str):
+    pass
+
+
+def update_setup_py(new_version: str):
+    pass
+
+
+if __name__ == '__main__':
+    user_version = input('Version to set > ')
