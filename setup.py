fec8b212fb53427d616f28c3a9af31834d5b0a76
Peter Urda <peter.urda@gmail.com>
2015-11-26 00:37:32 -0500

    TESTING A TRAVIS BUILD BREAK

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='py_nist_beacon',
     packages=['py_nist_beacon'],
-    version='0.1.6',
+    version='travis build break',
 
     description='Python 3 Library to access the NIST Randomness Beacon',
     long_description=open('README.rst').read(),
