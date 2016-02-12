77e8fc067e33c96faa475d3c14819eb374cb7d70
Peter Urda <peter.urda@gmail.com>
2016-02-12 00:21:28 -0800

    Single test, verbose for now

--- tests/integration/test_nist_integration.py
+++ tests/integration/test_nist_integration.py
@@ -1,18 +1,32 @@
 from unittest import TestCase
 
+from nistbeacon import (
+    NistBeacon,
+    NistBeaconValue,
+)
+
 
 class TestNistIntegration(TestCase):
     def setUp(self):
-        pass
+        self.target_timestamp = 1447873020
 
-    def test_get_last_record(self):
-        pass
-
-    def test_get_next(self):
-        pass
-
-    def test_get_previous(self):
-        pass
+    # def test_get_last_record(self):
+    #     pass
+    #
+    # def test_get_next(self):
+    #     pass
+    #
+    # def test_get_previous(self):
+    #     pass
 
     def test_get_record(self):
-        pass
+        downloaded = NistBeacon.get_last_record()
+
+        self.assertIsInstance(
+            obj=downloaded,
+            cls=NistBeaconValue,
+        )
+
+        self.assertTrue(
+            downloaded.valid_signature,
+        )
